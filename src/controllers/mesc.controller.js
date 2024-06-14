e7e400f74ea2a90cb8706a31acefe477b0d42787
ygorxds <ygordesa2010@hotmail.com>
2024-06-14 20:00:54 -0300

    feat: paroquia processamento

--- src/controllers/mesc.controller.js
+++ src/controllers/mesc.controller.js
@@ -1,6 +1,6 @@
 const createKnex = require('../context')
-
 const knex = createKnex();
+const moment = require('moment');
 
 const MescController = {
     async buscar(req, res) {
@@ -23,21 +23,31 @@ const MescController = {
    
     async inserirMesc(req, res) {
         const mesc = req.body;
-        try{
-        let result = await knex('mescs').insert(mesc);
-        if(!result) return res.status(400).json({msg:'Mesc does not inserted'});
-        return res.status(200).json({msg:'Mesc inserted'});
-} catch {
-    return res.status(500).json({msg:'Mesc not inserted'})
-}
-
+        try {
+            mesc.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+            mesc.updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
+            let result = await knex('mescs').insert(mesc);
+            if (!result) return res.status(400).json({ msg: 'Mesc does not inserted' });
+            return res.status(200).json({ msg: 'Mesc inserted' });
+        } catch (error) {
+            console.error('Error inserting Mesc:', error);
+            return res.status(500).json({ msg: 'Mesc not inserted' });
+        }
     },
+
     async editarMesc(req, res) {
         const { id } = req.params;
         const mesc = req.body;
-        let result = await knex('mescs').where({ id }).update(mesc);
-        if (!result) return res.status(400).json({msg: 'Mesc does not updated'});
-        return res.status(200).json({msg: 'Mesc updated'});
+        try {
+            mesc.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
+            mesc.updated_at = moment().format('YYYY-MM-DD HH:mm:ss');
+            let result = await knex('mescs').where({ id }).update(mesc);
+            if (!result) return res.status(400).json({ msg: 'Mesc does not updated' });
+            return res.status(200).json({ msg: 'Mesc updated' });
+        } catch (error) {
+            console.error('Error updating Mesc:', error);
+            return res.status(500).json({ msg: 'Mesc not updated' });
+        }
     },
 
     async deletarMesc(req, res) {
