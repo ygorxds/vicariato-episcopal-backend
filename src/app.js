e80bc5a26c15eac18f60d3df1429182db201751e
ygorxds <ygordesa2010@hotmail.com>
2024-06-19 19:43:19 -0300

    debbug: debbug cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Configuração do CORS
+app.use(cors());
 
 app.use(express.json());
 
+const routes = require('./router/index.routes.js');
+app.use(routes);
 
 module.exports = app;
